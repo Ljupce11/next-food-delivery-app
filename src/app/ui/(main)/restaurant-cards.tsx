423520d8ded342b4c558de7fe618a8c88479741b
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-01-17 23:29:12 +0100

    Add loading page for restaurant page

--- src/app/ui/(main)/restaurant-cards.tsx
+++ src/app/ui/(main)/restaurant-cards.tsx
@@ -14,8 +14,8 @@ export default function RestaurantCards({ restaurants }: Props) {
   return (
     <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 px-8">
       {restaurants.map(({ id, name, image }) => (
-        <Card key={id} isFooterBlurred className="border-none" radius="lg" shadow="sm">
-          <Link href={`/restaurant/${id}`} key={id} prefetch={true}>
+        <Link href={`/restaurant/${id}`} key={id} prefetch={true}>
+          <Card key={id} isFooterBlurred className="border-none" radius="lg" shadow="sm">
             <Image isZoomed alt={name} className="object-cover" height={200} src={image} width={"100%"} />
             <CardFooter className="justify-between before:bg-black/20 bg-white/50 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
               <div className="flex justify-between items-center w-full gap-2">
@@ -31,8 +31,8 @@ export default function RestaurantCards({ restaurants }: Props) {
                 </div>
               </div>
             </CardFooter>
-          </Link>
-        </Card>
+          </Card>
+        </Link>
       ))}
     </div>
   );
