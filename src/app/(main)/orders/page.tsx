eb73165f4b28db88b3761434b75264cbff2e4c24
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-01-18 01:20:44 +0100

    Implement initial UI with mock data for Orders page

--- src/app/(main)/orders/page.tsx
+++ src/app/(main)/orders/page.tsx
@@ -1,7 +1,155 @@
-export default async function Page() {
+"use client";
+
+import { ClipboardDocumentListIcon } from "@heroicons/react/24/outline";
+import {
+  Card,
+  CardBody,
+  Chip,
+  type ChipProps,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  User,
+} from "@heroui/react";
+import React from "react";
+
+export const columns = [
+  { name: "RESTAURANT", uid: "restaurant" },
+  { name: "AGE", uid: "age" },
+  { name: "Team", uid: "team" },
+  { name: "ROLE", uid: "role" },
+  { name: "STATUS", uid: "status" },
+];
+
+export const users = [
+  {
+    id: 1,
+    name: "Tony Reichert",
+    role: "CEO",
+    team: "Management",
+    status: "completed",
+    age: "29",
+    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+    email: "tony.reichert@example.com",
+  },
+  {
+    id: 2,
+    name: "Zoey Lang",
+    role: "Technical Lead",
+    team: "Development",
+    status: "cancelled",
+    age: "25",
+    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
+    email: "zoey.lang@example.com",
+  },
+  {
+    id: 3,
+    name: "Jane Fisher",
+    role: "Senior Developer",
+    team: "Development",
+    status: "completed",
+    age: "22",
+    avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+    email: "jane.fisher@example.com",
+  },
+  {
+    id: 4,
+    name: "William Howard",
+    role: "Community Manager",
+    team: "Marketing",
+    status: "paused",
+    age: "28",
+    avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
+    email: "william.howard@example.com",
+  },
+  {
+    id: 5,
+    name: "Kristen Copper",
+    role: "Sales Manager",
+    team: "Sales",
+    status: "completed",
+    age: "24",
+    avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
+    email: "kristen.cooper@example.com",
+  },
+];
+
+const statusColorMap: Record<string, ChipProps["color"]> = {
+  completed: "success",
+  cancelled: "danger",
+};
+
+type UserType = (typeof users)[0];
+
+export default function Page() {
+  const renderCell = React.useCallback((user: UserType, columnKey: React.Key) => {
+    const cellValue = user[columnKey as keyof UserType];
+
+    switch (columnKey) {
+      case "restaurant":
+        return (
+          <User avatarProps={{ radius: "lg", src: user.avatar }} description={user.email} name={cellValue}>
+            {user.email}
+          </User>
+        );
+      case "date":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+            {cellValue}
+          </Chip>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
   return (
-    <div className="flex flex-col gap-8 mb-20">
-      <p>Orders</p>
+    <div className="container flex flex-col gap-5 mx-auto p-4">
+      <div className="flex items-center justify-center md:justify-start gap-3">
+        <Card shadow="sm">
+          <CardBody>
+            <ClipboardDocumentListIcon className="size-6" />
+          </CardBody>
+        </Card>
+        <h1 className="text-xl font-semibold">Your orders</h1>
+      </div>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+        {Array.from({ length: 4 }).map((_, i) => {
+          const id = i + 1;
+          return (
+            <Card key={id} shadow="sm">
+              <CardBody>
+                <p className="text-lg font-semibold">27</p>
+                <p>Total Orders</p>
+              </CardBody>
+            </Card>
+          );
+        })}
+      </div>
+      <Table isStriped aria-label="Example table with custom cells">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={users}>
+          {(item) => (
+            <TableRow key={item.id}>{(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}</TableRow>
+          )}
+        </TableBody>
+      </Table>
     </div>
   );
 }
