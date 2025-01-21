7b9951fdadf1a7b69b665339c6e96825e14c20c7
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-01-21 13:48:17 +0100

    Refactor restaurant page

--- src/app/(main)/restaurant/[id]/page.tsx
+++ src/app/(main)/restaurant/[id]/page.tsx
@@ -1,7 +1,18 @@
-import RestaurantPage from "@/app/ui/(main)/restaurant-page";
+import { fetchRestaurant } from "@/app/lib/data";
+import RestaurantPageMenu from "@/app/ui/(main)/restaurant-page-menu";
+import RestaurantPageSidebar from "@/app/ui/(main)/restaurant-page-sidebar";
+import { RestaurantPageMenuSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-
-  return <RestaurantPage restaurantId={id} />;
+  const restaurant = await fetchRestaurant(id);
+  return (
+    <div className="flex flex-col justify-around w-full px-8 py-5 gap-3 lg:flex-row">
+      <RestaurantPageSidebar restaurant={restaurant} />
+      <Suspense fallback={<RestaurantPageMenuSkeleton />}>
+        <RestaurantPageMenu restaurant={restaurant} />
+      </Suspense>
+    </div>
+  );
 }
